b28e46290be6635aae93bb243c260653053f6a4d
TS-SCRIPTS <103532607+TS-SCRIPTS@users.noreply.github.com>
2022-06-30 02:29:34 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'common'
+
+author 'TS#3462'
+
+description 'ESX Chat design by TS#3462'
+
+client_scripts {
+    'client/client.lua'
+}
+
+ui_page {
+    'ui/ui.html'
+}
+
+files {
+    '**/**/**/**/**/**/**/**/*.*'
+}
